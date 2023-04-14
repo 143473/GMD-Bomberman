159bd9187d275920144003ea756f90a7d20b0e27
Agostina-mr <agosmez@gmail.com>
2023-04-14 21:50:38 +0200

    the bomb explodes checking the cells using overlapbox

--- Bomberman/Assets/Scripts/BombScript.cs
+++ Bomberman/Assets/Scripts/BombScript.cs
@@ -8,9 +8,10 @@ public class BombScript : MonoBehaviour
 
     public float delay = 3f;
     private bool hasExploded;
-    public float radius = 5f;
-    public float force = 500;
     public BoxCollider bc;
+    public int blast = 1;
+
+    Vector3 halfExtent = new Vector3(0.5f, 0, 0.5f);
 
     void Start()
     {
@@ -21,7 +22,7 @@ public class BombScript : MonoBehaviour
     {
         delay -= Time.deltaTime;
         if (delay <= 0f && !hasExploded) {
-          Explode();
+         Explode();
         }
         if (!bc.enabled) {
            Collider[] colliders = Physics.OverlapSphere(transform.position, 0.5f);
@@ -30,20 +31,52 @@ public class BombScript : MonoBehaviour
            }
         }
     }
+  
+    void Explode()
+    {
+      CheckCell(transform.position);
+      
+      CheckDirection(Vector3.forward);
+      CheckDirection(Vector3.back);
+      CheckDirection(Vector3.left);
+      CheckDirection(Vector3.right);
+      
+      Destroy(gameObject);      
+      hasExploded = true;
+    }
 
-    void Explode(){
-      Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-      foreach(Collider nearbyWall in colliders) {
-        Rigidbody rb = nearbyWall.GetComponent<Rigidbody>();
-        if(rb != null) {
-          rb.AddExplosionForce(force, transform.position, radius);
-        }
-        Destructible wall = nearbyWall.GetComponent<Destructible>();
-        if (wall != null) {
-          wall.DestroyObject();
+    void CheckDirection(Vector3 direction)
+    {
+      for (int i = 1; i < blast + 1; i++)
+      {
+        //offset from the bomb position to look up the other cells 
+        Vector3 offset = direction * (i * 1.25f);
+        //we need to consider the bomb position as well
+        Vector3 cellPosition = transform.position + offset;
+
+        if (CheckCell(cellPosition))
+        {
+          break;
         }
       }
-      hasExploded = true;
-      Destroy(gameObject);
+    }
+
+    bool CheckCell(Vector3 cellPosition)
+    {
+      //check the colliders in the cell 
+      Collider[] colliders = Physics.OverlapBox(cellPosition, halfExtent, Quaternion.identity);
+      Debug.Log($"Pos {transform.position} Cell {cellPosition} Colliders {colliders.Length}");
+
+      foreach (Collider collider in colliders)
+      {
+        Debug.Log($"Collider {collider.transform.position}");
+          
+        Destructible destructible = collider.GetComponent<Destructible>();
+        if (destructible != null) {
+          destructible.DestroyObject();
+        }
+      }
+
+      return colliders.Length > 0;
     }
 }
