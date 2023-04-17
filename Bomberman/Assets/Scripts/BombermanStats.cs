8290073731af35a2bf03a1d7949b51ae7dcfc9f2
Claudiu <75113646+143473@users.noreply.github.com>
2023-04-17 20:05:51 +0200

    addeds powerup interface and stats class for bomberman

---
+++ Bomberman/Assets/Scripts/BombermanStats.cs
@@ -0,0 +1,12 @@
+namespace DefaultNamespace
+{
+    public class BombermanStats
+    {
+        public int TotalNumberOfBombs { get; set; } = 1;
+        public int Explosion { get; set; } = 1;
+        public int Speed { get; set; } = 1;
+        public bool RemoteExplosion { get; set; } = false;
+        public bool Kick { get; set; } = false;
+        public bool Curse { get; set; } = false;
+    }
+}
