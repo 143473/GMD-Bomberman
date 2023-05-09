f2ac42890a76bba3a1e04bb406cae965de9202bc
Claudiu <75113646+143473@users.noreply.github.com>
2023-05-09 11:15:15 +0200

    fix

--- Bomberman/Assets/Scripts/Managers/PlayerManager.cs
+++ Bomberman/Assets/Scripts/Managers/PlayerManager.cs
@@ -160,7 +160,7 @@ namespace Managers
             var canvas = Instantiate(canvasP);
             canvas.transform.position = new Vector3(x, 1, z);
             canvas.name = playerName + " Canvas";
-            canvas.GetComponent<Image>().color = color.WithAlphaMultiplied(0.3f);
+            canvas.GetComponent<Image>().color = color;
         }
     }
 }
